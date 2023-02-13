8ae87a003d10575be00bd468a813b35558885a1c
Tintu <Tintu@DESKTOP-IAMER04>
2023-02-13 16:57:32 +0530

    Linkedin

--- Scripts/LinkedIn/Script1676284531756.groovy
+++ Scripts/LinkedIn/Script1676284531756.groovy
@@ -19,6 +19,8 @@ import org.openqa.selenium.Keys as Keys
 
 WebUI.openBrowser('')
 
+WebUI.maximizeWindow()
+
 WebUI.navigateToUrl('www.linkedin.com')
 
 WebUI.click(findTestObject('LinkedIn/Page_LinkedIn Log In or Sign Up/a_Join now'))
