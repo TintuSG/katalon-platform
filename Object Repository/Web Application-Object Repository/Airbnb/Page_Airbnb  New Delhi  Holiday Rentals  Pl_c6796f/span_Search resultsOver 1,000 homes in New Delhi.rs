464dc22d03c88cdea3c6086ef00a293133b93384
Tintu <Tintu@DESKTOP-IAMER04>
2023-02-22 13:01:36 +0530

    Desktop Notepad/Calc

---
+++ Object Repository/Web Application-Object Repository/Airbnb/Page_Airbnb  New Delhi  Holiday Rentals  Pl_c6796f/span_Search resultsOver 1,000 homes in New Delhi.rs
@@ -0,0 +1,107 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>span_Search resultsOver 1,000 homes in New Delhi</name>
+   <tag></tag>
+   <elementGuidId>fb687e0d-142b-4736-a658-8ce06ba948a0</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>span.to8eev7.dir.dir-ltr</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//main[@id='site-content']/div[2]/div/div/div/div/div/section/h1/span</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>span</value>
+      <webElementGuid>f5f4454b-54bd-496e-be31-2e3289795070</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>to8eev7 dir dir-ltr</value>
+      <webElementGuid>9f5ae37b-edd3-4de9-99b2-d9e31c52b8fe</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Search resultsOver 1,000 homes in New Delhi</value>
+      <webElementGuid>fd775c72-2e2a-4dbc-b280-6138efcc4d32</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;site-content&quot;)/div[@class=&quot;fmdphkf dir dir-ltr&quot;]/div[@class=&quot;p1180s14 dir dir-ltr&quot;]/div[@class=&quot;c1yo0219 dir dir-ltr&quot;]/div[1]/div[@class=&quot;c1aasoj4 dir dir-ltr&quot;]/div[@class=&quot;t16kra9o dir dir-ltr&quot;]/section[1]/h1[@class=&quot;_14i3z6h&quot;]/span[@class=&quot;to8eev7 dir dir-ltr&quot;]</value>
+      <webElementGuid>34c9be17-ca7f-4811-ad57-d75faa915e60</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//main[@id='site-content']/div[2]/div/div/div/div/div/section/h1/span</value>
+      <webElementGuid>b92ef3a5-6d50-44c8-b535-aefeb46f5c4d</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Show map'])[1]/following::span[1]</value>
+      <webElementGuid>37491258-23dc-4128-b8b1-3790f7a6ff45</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Filters'])[1]/following::span[3]</value>
+      <webElementGuid>74fbfc39-c8aa-4851-abac-aa7b7bb1bf66</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Apartment in New Delhi'])[1]/preceding::span[14]</value>
+      <webElementGuid>747d3f63-a760-48e7-9b75-194fb2c76a10</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Over 1,000 homes in New Delhi']/parent::*</value>
+      <webElementGuid>51749dfe-4aeb-4f8f-b7ab-629ee319871d</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//h1/span</value>
+      <webElementGuid>18f85c06-3f8f-4eaa-a629-cbe52fc44cd8</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//span[(text() = 'Search resultsOver 1,000 homes in New Delhi' or . = 'Search resultsOver 1,000 homes in New Delhi')]</value>
+      <webElementGuid>97df8383-dfd1-43ae-b9d5-16b5aaadbcc7</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Object Repository/Web Application-Object Repository/Ajio/Page_Buy Black Jeans for Men by iVOC Online_f23005/span_ADD TO BAG.rs
@@ -0,0 +1,106 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>span_ADD TO BAG</name>
+   <tag></tag>
+   <elementGuidId>18490576-5305-46a4-a082-1bc3236a9107</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='appContainer']/div[2]/div/div/div[2]/div/div[3]/div/div[9]/div/div/div/span[2]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>span</value>
+      <webElementGuid>d5bd0c44-4f85-470f-b242-abe68d9f3ec5</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>ADD TO BAG</value>
+      <webElementGuid>c452dab0-3b99-40f2-8263-316f0997705c</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;appContainer&quot;)/div[@class=&quot;content&quot;]/div[1]/div[@class=&quot;main-view product-view false&quot;]/div[@class=&quot;prod-container&quot;]/div[@class=&quot;row&quot;]/div[@class=&quot;col-4&quot;]/div[@class=&quot;prod-content&quot;]/div[@class=&quot;pdp-cart-buttons&quot;]/div[1]/div[@class=&quot;pdp-addtocart-button&quot;]/div[@class=&quot;btn-gold&quot;]/span[2]</value>
+      <webElementGuid>533e2724-3dcc-45da-8914-ff364e7f7d8a</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='appContainer']/div[2]/div/div/div[2]/div/div[3]/div/div[9]/div/div/div/span[2]</value>
+      <webElementGuid>963d7bcb-0687-444a-bdd3-6224b4b29937</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Select your size to know your estimated delivery date.'])[1]/following::span[2]</value>
+      <webElementGuid>566bb1d8-e557-4bf4-a65f-b4298bdbdb5a</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='NEW'])[5]/following::span[4]</value>
+      <webElementGuid>ac0bda18-70c4-4637-9ef5-fc4216405140</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='HANDPICKED STYLES | ASSURED QUALITY'])[1]/preceding::span[1]</value>
+      <webElementGuid>0eb7a2d2-5d1d-4aa5-8318-75a89d5c2bec</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='SAVE TO WISHLIST'])[1]/preceding::span[1]</value>
+      <webElementGuid>1fbbc82c-9a6a-418d-b7bb-88e3fa191d85</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='ADD TO BAG']/parent::*</value>
+      <webElementGuid>e5a7ed99-efcc-4ac6-bc69-145e55daf10e</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[9]/div/div/div/span[2]</value>
+      <webElementGuid>6c4fa1ef-c566-43cc-9a68-8de61e2ad798</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//span[(text() = 'ADD TO BAG' or . = 'ADD TO BAG')]</value>
+      <webElementGuid>d0ea264c-ddc7-488c-887e-c818c1f2f937</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
